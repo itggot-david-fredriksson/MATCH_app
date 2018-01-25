9255cfb78ec5e90a8940bedc72ee2ca7714e9bd5
itggot-david-fredriksson <David.fredriksson@itggot.se>
2018-01-25 09:47:02 +0100

    Initial commit

---
+++ app.rb
@@ -0,0 +1,62 @@
+class App < Sinatra::Base
+	
+	enable :sessions
+
+	get '/' do
+		slim(:index)
+	end
+
+	get('/register') do
+		slim(:register)
+	end
+
+			
+
+	post('/register') do
+		db = SQLite3::Database.new('db/match.sqlite')
+		db.results_as_hash = true
+		
+		username = params["username"]
+		password = params["password"]
+		password_confirmation = params["confirm_password"]
+		
+		result = db.execute("SELECT id FROM users WHERE username=?", [username])
+
+		if result.empty?
+			if password == password_confirmation
+				password_digest = BCrypt::Password.create(password)
+				
+				db.execute("INSERT INTO users(username, password_digest) VALUES (?,?)", [username, password_digest])
+				redirect('/')
+			else
+				set_error("Passwords don't match")
+				redirect('/error')
+			end
+		else
+			set_error("Username already exists")
+			redirect('/error')
+		end
+
+	end
+
+	post('/login') do
+		db = SQLite3::Database.new('db/match.sqlite')
+		db.results_as_hash = true
+		username = params["username"]
+		password = params["password"]
+		
+		result = db.execute("SELECT id, password_digest FROM users WHERE username=?", [username])
+		
+		user_id = result.first["id"]
+		password_digest = result.first["password_digest"]
+		if BCrypt::Password.new(password_digest) == password
+			session[:user_id] = user_id
+			redirect('/notes')
+		else
+			set_error("Invalid Credentials")
+			redirect('/error')
+		end
+	end
+
+
+end
